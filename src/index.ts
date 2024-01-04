3593941b29df6548d9e382fd4e3365313935ffb0
Namean <dwilsoncnc@gmail.com>
2024-01-04 07:00:54 -0500

    Completed 92. sorting arbitrary collections

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersColelction = new NumbersCollection([50, 10, 3, -5, 0]);
-const sorter = new Sorter(numbersColelction);
+
+// const numbersColelction = new NumbersCollection([50, 10, 3, -5, 0]);
+// const sorter = new Sorter(numbersColelction);
+// sorter.sort();
+// console.log(numbersColelction.data);
+
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersColelction.data);
+console.log(charactersCollection.data);
