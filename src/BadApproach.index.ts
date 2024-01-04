65b527f5628aeb4873e48de85b9507dd38162d02
Namean <dwilsoncnc@gmail.com>
2024-01-03 22:01:16 -0500

    Completed 85. TypeScript is really smart

---
+++ src/BadApproach.index.ts
@@ -0,0 +1,26 @@
+class Sorter2 {
+  //   collection: number[];
+
+  constructor(public collection: number[]) {
+    this.collection = collection;
+  }
+
+  // methods
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - (i - 1); j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter2 = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
