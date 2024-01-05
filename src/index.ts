44e4429d0fb8e91bf5aae0c89ce59f7ca1b244a5
Namean <dwilsoncnc@gmail.com>
2024-01-05 03:07:33 -0500

    Completed 95. just one more fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-
+import { LinkedList } from "./LinkedList";
 
 // const numbersColelction = new NumbersCollection([50, 10, 3, -5, 0]);
 // const sorter = new Sorter(numbersColelction);
 // sorter.sort();
 // console.log(numbersColelction.data);
 
+// const charactersCollection = new CharactersCollection("Xaayb");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
