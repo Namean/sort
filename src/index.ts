e01ee531fa47c36fcdfd330905c96477ff6230a4
Namean <dwilsoncnc@gmail.com>
2024-01-31 23:30:27 -0500

    removed some old code

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([50, 10, 3, -5, 0]);
-// numbersCollection.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([50, 10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
 charactersCollection.sort();
